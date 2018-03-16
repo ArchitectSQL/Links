17db22a4c2b294d49dbb4ffbed1e7748b4dbfedf
Evgeniy <magentodev19@gmail.com>
2018-03-16 19:32:30 +0200

    Url 95% done

---
+++ Url/Plugin/Model/CategoryUrlPathGenerator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev01
+ * Date: 16.03.18
+ * Time: 12:03
+ */
+namespace Web4pro\Url\Plugin\Model;
+
+use Web4pro\Url\Model\CatalogUrlRewrite\ProductUrlPathGenerator;
+
+class CategoryUrlPathGenerator
+{
+    /**
+     * @param \Magento\CatalogUrlRewrite\Model\CategoryUrlPathGenerator $subject
+     * @param $path
+     * @return string
+     */
+    public function afterGetUrlPath(\Magento\CatalogUrlRewrite\Model\CategoryUrlPathGenerator $subject, $path)
+    {
+        //var_dump($subject);
+        var_dump($path);
+        if(strpos($path, ProductUrlPathGenerator::CATEGORY_PREFIX_ROUTE) === false)
+           $path = ProductUrlPathGenerator::CATEGORY_PREFIX_ROUTE . $path;
+        var_dump($path);//exit();
+        return $path;
+    }
+}
