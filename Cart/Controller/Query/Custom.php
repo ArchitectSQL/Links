d6b7b1a839fee2028d65d936500a3e4207cf4831
Evgeniy <magentodev19@gmail.com>
2018-03-28 18:52:08 +0300

    ajax done, need fix bug

--- Cart/Controller/Query/Custom.php
+++ Cart/Controller/Query/Custom.php
@@ -5,25 +5,62 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 class Custom extends \Magento\Framework\App\Action\Action
 {
-    protected $resultJsonFactory;
 
+    /**
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $_quoteRepository;
+    
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * @var \Magento\Checkout\Model\Cart
+     */
+    protected $cart;
+
+    /**
+     * @var ResultFactory
+     */
+    protected $resultJsonFactory;
+    
     public function __construct(
         Context  $context,
-        ResultFactory $resultJsonFactory
+        ResultFactory $resultJsonFactory,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Checkout\Model\Cart $cart
 
     ) {
+        $this->cart = $cart;
+        $this->_checkoutSession = $checkoutSession;
+        $this->_quoteRepository = $quoteRepository;
         $this->resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
     }
 
-        
- 
+    /**
+     * @return string
+     */
     public function execute()
     {
+        $priceTotal = null;
+        $itemId = (int)$this->getRequest()->getPost('idItem');
+        $qty = (int)$this->getRequest()->getPost('qty');
 
-        var_dump($this->getRequest()->getPost());exit();
-        $resultJson = $this->resultJsonFactory->create(ResultFactory::TYPE_JSON);
-        return $resultJson->setData(['success' => 'test']);
+        $cartitems = $this->cart->getItems();
+        foreach ($cartitems as $cartitem) {
+            if ($cartitem->getProduct()->getId() == $itemId)
+            {
+                $price = (int)$cartitem->getPrice();
+                $priceTotal = $price * $qty;
+                $cartitem->setQty($qty)->setRowTotal($priceTotal)->save();
+            }
+        }
+        //$resultJson = $this->resultJsonFactory->create(ResultFactory::TYPE_JSON);
+        echo $priceTotal;
         
     }
 }
--- Cart/view/frontend/web/js/web4pro-cart.js
+++ Cart/view/frontend/web/js/web4pro-cart.js
@@ -6,38 +6,49 @@ define([
     $.widget('web4pro.cart', {
         options: {
             triggerEvent: 'change',
-            controller: 'http://developer.loc/web4pro_cart/query/custom'
+            controller: 'http://developer.loc/web4pro_cart/query/custom',
+            qty: '[data-role="cart-item-qty"]',
+            itemId: '.action.action-edit'
         },
 
         _create: function() {
-            console.log('3333');
             this._bind();
         },
 
         _bind: function() {
             var self = this;
             self.element.on(self.options.triggerEvent, function() {
-                console.log('2222');
                 self._ajaxSubmit();
-
             });
-
         },
+
         _ajaxSubmit: function() {
-            console.log(jQuery('[data-role="cart-item-qty"]').val());
-            console.log('1111');
-            console.log(this.options.controller);
-            jQuery.ajax({
-                url: this.options.controller,
-                type: 'post',
-                dataType: 'json',
-                data: 'qty='+jQuery('[data-role="cart-item-qty"]').val(),
-                success: function(res) {
-                    alert('ajax send');
-                    console.log('ajax success');
-                    console.log(JSON.stringify(res));
-                }
+            jQuery(this.options.qty).on('change', function () {
+                var that = this;
+                var qty = $(this).val();
+                var editAction = $(this).closest('tbody.cart.item').find('a.action-edit').attr('href');
+                var arrayHerf = editAction.split('/');
+                var blank = arrayHerf.pop();
+                var idItem = arrayHerf.pop();
+                console.log(qty);
+                console.log(idItem);
+                jQuery.ajax({
+                    url: 'http://developer.loc/web4pro_cart/query/custom',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {qty: qty,idItem: idItem},
+                    success: function(res) {
+                        console.log('ajax success');
+                        var price = JSON.stringify(res);
+                        var totalPrice = $(that).closest('tbody.cart.item').find('.subtotal span.price');
+                        totalPrice.text('$' + price);
+                    }
+                });
             });
+        },
+
+        _updateOrderHandler: function () {
+            $(this).trigger('change');
         }
     });
     return $.web4pro.cart;
